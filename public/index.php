1ddc90d47b9a23e98848d33331a7617f76222b6d
paliparsa <121433935+paliparsa@users.noreply.github.com>
2026-07-25 04:29:36 +0300

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
 $host = strtolower($_SERVER['HTTP_HOST'] ?? '');
 
 // If domain is Telegram Bot domain, route to Telegram MiniApp:
